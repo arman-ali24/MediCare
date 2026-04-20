fb6de52f45345da5e8307a32f5e853fed0b82ffb
Arman Ali <armanali0178614@gmail.com>
2026-04-20 12:12:58 +0530

    add dashboard panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Hero from "./pages/Hero";
 import { useUser } from "@clerk/clerk-react";
-import { Link } from "react-router-dom";
-import { Home } from "lucide-react";
+import Home from "./pages/Home";
 
 function RequireAuth({ children }) {
   const { isLoaded, isSignedIn } = useUser();
 
   if (!isLoaded) return null;
+
   if (!isSignedIn)
     return (
       <div
-        className="min-h-screen font-mono flex items-center justify-center bg-linear-to-b
+        className="min-h-screen font-mono flex items-center justify-center bg-gradient-to-b
        from-emerald-50 via-green-50 to-emerald-100 px-4"
       >
         <div className="text-center">
-          <p className="text-emerald-800 font-semibold text-lg sm:text-2xl mb-4 animate-fade-in">
+          <p className="text-emerald-800 font-semibold text-lg sm:text-2xl mb-4">
             Please sign in to view this page
           </p>
 
           <div className="flex justify-center">
             <Link
               to="/"
-              className="px-4 py-2 text-sm rounded-full bg-emerald-600 text-white shadow-sm hover:bg-emerald-700 hover:shadow-md transition-all duration-300 ease-in-out animate-bounce-subtle"
+              className="px-4 py-2 text-sm rounded-full bg-emerald-600 text-white shadow-sm hover:bg-emerald-700 hover:shadow-md transition-all duration-300 ease-in-out"
             >
               HOME
             </Link>
@@ -31,6 +31,7 @@ function RequireAuth({ children }) {
         </div>
       </div>
     );
+
   return children;
 }
 
