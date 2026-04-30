081733e1b050f0c209556599d712d3c44df39596
Arman Ali <armanali0178614@gmail.com>
2026-05-01 01:16:36 +0530

    creating servicepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import DoctorDetail from "./pages/DoctorDetail";
+import Service from "./pages/Service";
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/doctors/:id" element={<DoctorDetail />} />
+
+        <Route path="/services" element={<Service />} />
       </Routes>
     </div>
   );
